919602cf450c20566c0e2f0fbaca331a7a00a42b
moms-spaghetti <williamedwards36@aol.com>
2020-11-10 11:43:19 +0000

    updates

--- model/index.js
+++ model/index.js
@@ -1,8 +1,8 @@
-const { query } = require("../db/scripts/poolConnect.js");
+const { query } = require("../db/scripts/poolConnect");
 
 module.exports = {
   getAllUserData: async () => {
-    const sql = "SELECT * FROM users";
+    const sql = 'SELECT * FROM users';
     const res = await query(sql);
     return res.rows;
   },
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { getAllUserData } = require("../model/index");
 
-router.get("/", async function (req, res) {
+router.get("/", async function (req, res, next) {
   const allUsers = await getAllUserData();
   res.json({ payload: allUsers });
 });
