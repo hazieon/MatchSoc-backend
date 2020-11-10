3b5b8b8134a956720847d36af4d5dde78f604e14
kawalpreet <xx-sona-xx@live.co.uk>
2020-11-10 11:57:18 +0000

    models

--- model/index.js
+++ model/index.js
@@ -2,8 +2,14 @@ const { query } = require("../db/scripts/poolConnect");
 
 module.exports = {
   getAllUserData: async () => {
-    const sql = 'SELECT * FROM users';
+    const sql = "SELECT * FROM users";
     const res = await query(sql);
+    return res.rows;
+  },
+  getSpecificUser: async (user) => {
+    const sql =
+      "SELECT * FROM users WHERE CONCAT (firstname, ' ', surname) LIKE $1";
+    const res = await query(sql, [`%%${user.toLowerCase()}%%`]);
     return res.rows;
   },
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllUserData } = require("../model/index");
+const { getAllUserData, getSpecificUser } = require("../model/index");
 
 router.get("/", async function (req, res, next) {
+  if (req.query.search) {
+    const specificUser = await getSpecificUser(req.query.search);
+    res.json({ payload: specificUser });
+    return;
+  }
+
   const allUsers = await getAllUserData();
   res.json({ payload: allUsers });
 });
