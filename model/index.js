54118b8ee5ab41b3cb1fbdf2f6fef4f3c6bcfea7
moms-spaghetti <williamedwards36@aol.com>
2020-11-11 02:12:06 +0000

    sort data changes

--- model/index.js
+++ model/index.js
@@ -2,13 +2,13 @@ const { query } = require("../db/scripts/poolConnect");
 
 module.exports = {
   getAllUserData: async () => {
-    const sql = "SELECT * FROM users";
+    const sql = "SELECT * FROM users ORDER BY firstname ASC";
     const res = await query(sql);
     return res.rows;
   },
   getSpecificUser: async (user) => {
     const sql =
-      "SELECT * FROM users WHERE CONCAT (firstname, ' ', surname) LIKE $1";
+      "SELECT * FROM users WHERE CONCAT (firstname, ' ', surname) LIKE $1 ORDER BY firstname ASC";
     const res = await query(sql, [`%%${user.toLowerCase()}%%`]);
     return res.rows;
   },
