79a4592e6cff952f300f826bf58d4434ae230601
Hazieon <handrew3721@gmail.com>
2020-11-13 09:27:50 +0000

    base data

---
+++ db/scripts/data/testdataa.js
@@ -0,0 +1,333 @@
+const testdataa = [{
+  "firstname": "Aftab",
+  "surname": "Hussain",
+  "address": "147 Broadway REDHILL RH68 0 ZK ",
+  "email": "Aftab@gmail.com",
+  "phone": "01327892",
+  "image": "https://images.unsplash.com/photo-1552774021-9ebbb764f03e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "isbootcamper": true,
+  "industry": "management",
+  "interests": ["music", "programming", "travel", "family"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Alice",
+  "surname": "Holden",
+  "address": "714 The Drive  KINGSTON UPON THAMES  KT69 4ZI",
+  "email": "Alice@gmail.com",
+  "phone": "022358232",
+  "image": "https://images.unsplash.com/photo-1567336273898-ebbf9eb3c3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+  "isbootcamper": true,
+  "industry": "bank",
+  "interests": ["programming", "netflix", "plants", "running"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Arshi",
+  "surname": "Sheikh",
+  "address": "5 South Street ROCHESTER  ME21 1PB",
+  "email": "Arshi@gmail.com",
+  "phone": "03572359873",
+  "image": "https://images.unsplash.com/photo-1595664248149-94356b1ae05c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "isbootcamper": true,
+  "industry": "finance",
+  "interests": ["korean films", "programming", "technology"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Becky",
+  "surname": "Jones",
+  "address": "9472 Springfield Road   LUTON  LU28 4OU",
+  "email": "Becky@gmail.com",
+  "phone": "03523534",
+  "image": "https://images.unsplash.com/photo-1602622021975-dacdcf516c43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+  "isbootcamper": true,
+  "industry": "childcare",
+  "interests": ["films", "games", "nintendo", "disney", "programming"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Bradley",
+  "surname": "Smith",
+  "address": "33 York Road EAST CENTRAL LONDON  EC68 4SM",
+  "email": "Bradley@gmail.com",
+  "phone": "03523753434",
+  "image": "https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+  "isbootcamper": true,
+  "industry": "finance",
+  "interests": ["surfing", "programming", "running", "coffee", "cake", "pets"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Dan",
+  "surname": "Watson",
+  "address": "8 Chester Road   MILTON KEYNES   MK62 9JE",
+  "email": "DanWatson@gmail.com",
+  "phone": "03472039573",
+  "image": "https://images.unsplash.com/photo-1584418879404-85eb6c39c30c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "isbootcamper": true,
+  "industry": "fitness",
+  "interests": ["sports", "jokes", "family", "brownies", "exercise"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Emma",
+  "surname": "Randle",
+  "address": "95 Park Lane   OUTER HEBRIDES   HS96 7ZO",
+  "email": "Emma@gmail.com",
+  "phone": "0349283743",
+  "image": "https://images.unsplash.com/photo-1584308972272-9e4e7685e80f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+  "isbootcamper": true,
+  "industry": "paleontology",
+  "interests": ["baking", "dinosaurs", "cats", "technology", "reading"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Hajoo",
+  "surname": "Chung",
+  "address": "38 The Grove  DORCHESTER  DT90 6NS",
+  "email": "Hajoo@gmail.com",
+  "phone": "03535353",
+  "image": "https://images.unsplash.com/photo-1528341866330-07e6d1752ec2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": true,
+  "industry": "engineering",
+  "interests": ["maths", "Korean", "reading"],
+  "matchedwith": "not currently matched with a mentor"
+},
+
+{
+  "firstname": "Ismail",
+  "surname": "Ali",
+  "address": "31 Albert Road   TUNBRIDGE WELLS  TN97 1UC",
+  "email": "Ismail@gmail.com",
+  "phone": "043645345454",
+  "image": "https://images.unsplash.com/photo-1552266080-9db174b37b88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "isbootcamper": true,
+  "industry": "secret agent",
+  "interests": ["disappearing", "scuba diving", "board games"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Jeremy",
+  "surname": "Law",
+  "address": "553 Queensway   BRISTOL   BS47 0SH",
+  "email": "Jeremy@gmail.com",
+  "phone": "35343544",
+  "image": "https://images.unsplash.com/photo-1563254984-b465e6db2dcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": true,
+  "industry": "maths",
+  "interests": [
+    "saunas",
+    "gaming",
+    "maths",
+    "basketball",
+    "programming",
+    "running"
+  ],
+  "matchedwith": "not currently matched with a mentor"
+},
+
+{
+  "firstname": "Katie",
+  "surname": "Hawcutt",
+  "address": "493 School Lane  WAKEFIELD  WF42 9UT",
+  "email": "Katie@gmail.com",
+  "phone": "53453454",
+  "image": "https://images.unsplash.com/photo-1513091550446-33297bfca05b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": true,
+  "industry": "musician",
+  "interests": ["music", "parties", "programming", "films"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Maxine",
+  "surname": "Tillman",
+  "address": "88 St. John’s Road SWANSEA  SA81 0LS",
+  "email": "Maxine@gmail.com",
+  "phone": "453453454",
+  "image": "https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": true,
+  "industry": "cruise ships",
+  "interests": ["travel", "parties", "music", "dancing"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Kawalpreet",
+  "surname": "Kaur",
+  "address": "92 Kingsway READING  RG3 1LJ",
+  "email": "Kawalpreet@gmail.com",
+  "phone": "35325453",
+  "image": "https://images.unsplash.com/photo-1542611436-4d894c88d240?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": true,
+  "industry": "finance",
+  "interests": ["fintech", "parties", "music", "dancing"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Tom",
+  "surname": "Bennet",
+  "address": "37 Green Lane  GLOUCESTER  GL78 7SW",
+  "email": "TomB@gmail.com",
+  "phone": "3523523",
+  "image": "https://images.unsplash.com/photo-1600535597130-07a3c1fb40f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+  "isbootcamper": true,
+  "industry": "lifeguard",
+  "interests": ["swimming", "sports", "memes", "among us", "gaming"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Will",
+  "surname": "Edwards",
+  "address": "58 Stanley Road TRURO TR45 1DB",
+  "email": "Ani@gmail.com",
+  "phone": "63463464",
+  "image": "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": true,
+  "industry": "ICT",
+  "interests": ["programming", "games", "rpg", "cats"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Hazie",
+  "surname": "A",
+  "address": "73 Maple Road  NOTTINGHAM  NG37 0EH",
+  "email": "Hazie@gmail.com",
+  "phone": "37373737",
+  "image": "https://images.unsplash.com/photo-1561810051-503ed3ef1089?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": true,
+  "industry": "teaching",
+  "interests": ["travel", "illustration", "taekwondo", "flute", "programming"],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Zaid",
+  "surname": "Qarout",
+  "address": "73 Windsor Road   SLOUGH   SL59 8XU",
+  "email": "criminal@gmail.com",
+  "phone": "0101010101",
+  "image": "https://images.unsplash.com/photo-1576863151139-66451365be5e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "isbootcamper": true,
+  "industry": "designer",
+  "interests": [
+    "photoshop",
+    "astronomy",
+    "travel",
+    "technology",
+    "photography"
+  ],
+  "matchedwith": "not currently matched with a mentor"
+}, {
+  "firstname": "Ashleigh",
+  "surname": "Danks",
+  "address": "41 Manor Road  SWANSEA  SA22 5HL",
+  "email": "Ash@gmail.com",
+  "phone": "353526363",
+  "image": "https://images.unsplash.com/photo-1568344559488-28f10db78280?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+  "isbootcamper": false,
+  "industry": "software engineer",
+  "interests": ["programming", "memes", "technology", "astronomy"],
+  "matchedwith": "not currently matched with a bootcamper"
+}, {
+  "firstname": "Olivia",
+  "surname": "Winteringham ",
+  "address": "51 The Crescent  STEVENAGE  SG98 4NZ",
+  "email": "Winteringham@gmail.com",
+  "phone": "9924848383",
+  "image": "https://images.unsplash.com/photo-1598464831066-c911e7682a7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": false,
+  "industry": "designer",
+  "interests": ["UX design", "wellness", "fitness"],
+  "matchedwith": "not currently matched with a bootcamper"
+}, {
+  "firstname": "Josh",
+  "surname": "Farrant",
+  "address": "51 Park Avenue   KIRKCALDY   KY32 1MS",
+  "email": "Farrant@gmail.com",
+  "phone": "35352346",
+  "image": "https://images.unsplash.com/photo-1567752316810-57ad60ca5d50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "isbootcamper": false,
+  "industry": "developer",
+  "interests": ["programming", "technology", "korean films"],
+  "matchedwith": "not currently matched with a bootcamper"
+}, {
+  "firstname": "Sean",
+  "surname": "Kavanagh",
+  "address": "5 George Street BRISTOL  BS63 4JY",
+  "email": "Kavanagh@gmail.com",
+  "phone": "034025252",
+  "image": "https://unsplash.com/photos/IMYvZjlX3jE",
+  "isbootcamper": false,
+  "industry": "developer",
+  "interests": ["gaming", "cats", "cake", "coffee", "piano"],
+  "matchedwith": "not currently matched with a bootcamper"
+},
+
+{
+  "firstname": "Tajinder",
+  "surname": "Singh",
+  "address": "90 Victoria Road NEWPORT NP54 1VB",
+  "email": "Singh@gmail.com",
+  "phone": "0352352322",
+  "image": "https://images.unsplash.com/photo-1534987658209-443e1e80c443?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": false,
+  "industry": "databases",
+  "interests": ["technology", "piano", "programming", "running", "pets"],
+  "matchedwith": "not currently matched with a bootcamper"
+},
+
+{
+  "firstname": "Richard",
+  "surname": "Wallman",
+  "address": "97 The Green  LUTON LU71 4SB",
+  "email": "Wallman@gmail.com",
+  "phone": "035343",
+  "image": "https://images.unsplash.com/photo-1485206283729-2ca7d035185e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": false,
+  "industry": "developer",
+  "interests": ["programming", "plants", "netflix", "reading", "running"],
+  "matchedwith": "not currently matched with a bootcamper"
+},
+
+{
+  "firstname": "Amy",
+  "surname": "Hooper",
+  "address": "9151 King Street  COVENTRY   CV49 1VO",
+  "email": "Hooper@gmail.com",
+  "phone": "03634634",
+  "image": "https://images.unsplash.com/photo-1499887142886-791eca5918cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": false,
+  "industry": "developer",
+  "interests": ["cats", "baking", "reading", "programming", "running"],
+  "matchedwith": "not currently matched with a bootcamper"
+}, {
+  "firstname": "Sam",
+  "surname": "Dent",
+  "address": "792 Chester Road  OLDHAM   OL16 4DV",
+  "email": "Dent@gmail.com",
+  "phone": "05745645",
+  "image": "https://images.unsplash.com/photo-1578472577660-6f4a47a6660d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": false,
+  "industry": "developer",
+  "interests": ["pets", "games", "films", "nintendo", "baking"],
+  "matchedwith": "not currently matched with a bootcamper"
+},
+
+{
+  "firstname": "Clare",
+  "surname": "Streets",
+  "address": "5 Queens Road HARROGATE  HG67 2AV",
+  "email": "Clare@gmail.com",
+  "phone": "0784563453",
+  "image": "https://images.unsplash.com/photo-1530650068191-e80dcd5ee0b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": false,
+  "industry": "",
+  "interests": ["family", "travel", "music", "brownies", "baking", "exercise"],
+  "matchedwith": "not currently matched with a bootcamper"
+}, {
+  "firstname": "Omar",
+  "surname": "Qureshi",
+  "address": "7 Manchester Road  HALIFAX HX97 8TE",
+  "email": "@gmail.com",
+  "phone": "04634634",
+  "image": "https://images.unsplash.com/photo-1600707353232-0bae7afae565?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "isbootcamper": false,
+  "industry": "developer",
+  "interests": [
+    "travel",
+    "taekwondo",
+    "languages",
+    "games",
+    "running",
+    "programming"
+  ],
+  "matchedwith": "not currently matched with a bootcamper"
+}
+
+]
+module.exports = testdataa;
