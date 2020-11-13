e383d47a511e7ffe2e629d02ab0cf971407d5c40
Hazieon <handrew3721@gmail.com>
2020-11-13 09:28:34 +0000

    base data

--- db/scripts/populateTable.js
+++ db/scripts/populateTable.js
@@ -1,11 +1,11 @@
 const { query } = require("../scripts/poolConnect");
-const testdata = require("./data/testdata");
+const testdataa = require("./data/testdataa");
 
 const sql =
   "INSERT INTO users (firstname, surname, address, email, phone, image, isbootcamper, industry, interests, matchedwith) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
 
 function populateTable() {
-  testdata.map(async (value) => {
+  testdataa.map(async (value) => {
     const res = await query(sql, [
       value.firstname,
       value.surname,
